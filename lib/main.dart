cdeaeef0a779a4f95628239d68adae8aece9dec4
11mrhappy <nekomamiremy@gmail.com>
2021-02-02 22:34:06 +0900

    changed name

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,14 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<Album> fetchAlbum() async {
+Future<Connpass> fetchConnpass() async {
   final response =
   await http.get('https://jsonplaceholder.typicode.com/albums/1');
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
-    return Album.fromJson(jsonDecode(response.body));
+    return Connpass.fromJson(jsonDecode(response.body));
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
@@ -19,15 +19,15 @@ Future<Album> fetchAlbum() async {
   }
 }
 
-class Album {
+class Connpass {
   final int userId;
   final int id;
   final String title;
 
-  Album({this.userId, this.id, this.title});
+  Connpass({this.userId, this.id, this.title});
 
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
+  factory Connpass.fromJson(Map<String, dynamic> json) {
+    return Connpass(
       userId: json['userId'],
       id: json['id'],
       title: json['title'],
@@ -45,12 +45,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Future<Album> futureAlbum;
+  Future<Connpass> futureAlbum;
 
   @override
   void initState() {
     super.initState();
-    futureAlbum = fetchAlbum();
+    futureAlbum = fetchConnpass();
   }
 
   @override
@@ -65,7 +65,7 @@ class _MyAppState extends State<MyApp> {
           title: Text('Fetch Data Example'),
         ),
         body: Center(
-          child: FutureBuilder<Album>(
+          child: FutureBuilder<Connpass>(
             future: futureAlbum,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
