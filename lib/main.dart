0400486f47924701a882da30a740e1f59fa3e9cd
11mrhappy <nekomamiremy@gmail.com>
2021-02-02 22:44:06 +0900

    .

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,8 @@ Future<Connpass> fetchConnpass() async {
   await http.get('https://jsonplaceholder.typicode.com/albums/1');
 
   if (response.statusCode == 200) {
-    // If the server did return a 200 OK response,
-    // then parse the JSON.
     return Connpass.fromJson(jsonDecode(response.body));
   } else {
-    // If the server did not return a 200 OK response,
-    // then throw an exception.
     throw Exception('Failed to load album');
   }
 }
@@ -73,8 +69,6 @@ class _MyAppState extends State<MyApp> {
               } else if (snapshot.hasError) {
                 return Text("${snapshot.error}");
               }
-
-              // By default, show a loading spinner.
               return CircularProgressIndicator();
             },
           ),
