83e0856c7bf27a6dfd4516923b11fb84c01d3640
11mrhappy <nekomamiremy@gmail.com>
2021-02-03 20:21:13 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,26 +4,30 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-Future<Connpass> fetchConnpass() async {
+Future<Album> fetchAlbum() async {
   final response =
   await http.get('https://jsonplaceholder.typicode.com/albums/1');
 
   if (response.statusCode == 200) {
-    return Connpass.fromJson(jsonDecode(response.body));
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Album.fromJson(jsonDecode(response.body));
   } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
     throw Exception('Failed to load album');
   }
 }
 
-class Connpass {
+class Album {
   final int userId;
   final int id;
   final String title;
 
-  Connpass({this.userId, this.id, this.title});
+  Album({this.userId, this.id, this.title});
 
-  factory Connpass.fromJson(Map<String, dynamic> json) {
-    return Connpass(
+  factory Album.fromJson(Map<String, dynamic> json) {
+    return Album(
       userId: json['userId'],
       id: json['id'],
       title: json['title'],
@@ -41,12 +45,12 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Future<Connpass> futureConnpass;
+  Future<Album> futureAlbum;
 
   @override
   void initState() {
     super.initState();
-    futureConnpass = fetchConnpass();
+    futureAlbum = fetchAlbum();
   }
 
   @override
@@ -61,16 +65,16 @@ class _MyAppState extends State<MyApp> {
           title: Text('Fetch Data Example'),
         ),
         body: Center(
-          child: FutureBuilder<Connpass>(
-            future: futureConnpass,
+          child: FutureBuilder<Album>(
+            future: futureAlbum,
             builder: (context, snapshot) {
               if (snapshot.hasData) {
                 return Text(snapshot.data.title);
               } else if (snapshot.hasError) {
                 return Text("${snapshot.error}");
               }
-              print(snapshot.data.title);
 
+              // By default, show a loading spinner.
               return CircularProgressIndicator();
             },
           ),
